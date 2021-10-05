908f80ab3f0dd75f6d129dbec1fb5e5dff7c4f71
Yonathan Zerbib <50784530+yoni702@users.noreply.github.com>
2021-10-05 14:35:56 +0300

    kkk

---
+++ Prerequises_installs/Install_from_curl.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# InstallDocker.sh
+curl -fsSL -o InstallDocker.sh https://raw.githubusercontent.com/yoni702/FinalProject/master/Prerequises_installs/InstallDocker.sh?token=AMDOSESCYW6BRK4ILLQ7ZT3BLQ2PA
+chmod 700 InstallDocker.sh 
+./InstallDocker.sh 
+
+# InstallTerraform.sh
+curl -fsSL -o InstallTerraform.sh https://raw.githubusercontent.com/yoni702/FinalProject/master/Prerequises_installs/InstallTerraform.sh?token=AMDOSESCYW6BRK4ILLQ7ZT3BLQ2PA
+chmod 700 InstallTerraform.sh 
+./InstallTerraform.sh 
+
+
+#______Kubernetes_______
+
+# InstallEksctl.sh EKS
+curl -fsSL -o InstallEksctl.sh https://raw.githubusercontent.com/yoni702/FinalProject/master/Prerequises_installs/InstallEksctl.sh?token=AMDOSESCYW6BRK4ILLQ7ZT3BLQ2PA
+chmod 700 InstallEksctl.sh 
+./InstallEksctl.sh 
+
+# installMicrok8s.sh
+curl -fsSL -o installMicrok8s.sh https://raw.githubusercontent.com/yoni702/FinalProject/master/Prerequises_installs/installMicrok8s.sh?token=AMDOSESCYW6BRK4ILLQ7ZT3BLQ2PA
+chmod 700 installMicrok8s.sh 
+./installMicrok8s.sh 
+
+# InstallHelm.sh
+curl -fsSL -o InstallHelm.sh https://raw.githubusercontent.com/yoni702/FinalProject/master/Prerequises_installs/InstallHelm.sh?token=AMDOSESCYW6BRK4ILLQ7ZT3BLQ2PA
+chmod 700 InstallHelm.sh 
+./InstallHelm.sh 
+
+
+
+
+
+https://raw.githubusercontent.com/yoni702/FinalProject/1bb8acf195882dadc27d2b70a2017bffeded2c5d/Prerequises_installs/InstallDocker.sh?token=AMDOSEWJQG6KZB2B7BR6FMLBLQ4D6
