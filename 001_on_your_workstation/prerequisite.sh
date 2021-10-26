95a0cb2508c4bbd876d3c1c6c8bfb6f108a90639
Yonathan Zerbib <50784530+yoni702@users.noreply.github.com>
2021-10-26 08:53:23 +0300

    Prerequise

--- 001_on_your_workstation/prerequisite.sh
+++ 001_on_your_workstation/prerequisite.sh
@@ -6,37 +6,15 @@ read answer
 
 if [ "$answer" != "${answer#[Yy]}" ] ;then # this grammar (the #[] operator) means that the variable $answer where any Y or y in 1st position will be dropped if they exist.
     echo Yes
-    sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
-    # Add the HashiCorp GPG key.
-    curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-
-    # Add the official HashiCorp Linux repository.
-    sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-
-    # Update to add the repository, and install the Terraform CLI.
-    sudo apt-get update && sudo apt-get install terraform
-
-    # Verify the installation
-    terraform -help
-
-    # Then install the autocomplete package
-    touch ~/.bashrc
-    terraform -install-autocomplete 
-
-    # Check version of terraform
-    terraform --version
+    . InstallTerraform.sh
 else
-    echo No
+    echo Not installing terraform
 fi
 
-
-
-#install, initialize & clone git folder_______________________________________________________________________
-
-sudo snap install gh
-
-
 #ssh-keygen to mykey folder_______________________________________________________________________
+ssh-keygen -b 2048 -t rsa -f ../mykey/mykey -q -N ""
 
-
+# Terraform init in folder & Apply
+terraform -chdir="../terraform/" init 
+terraform -chdir="../terraform/" apply
 
--- terraform/securitygroup.tf
+++ terraform/securitygroup.tf
@@ -21,25 +21,3 @@ resource "aws_security_group" "allow-ssh" {
 }
 
 
-resource "aws_security_group" "allow-all-tcp" {
-  vpc_id      = aws_vpc.masteroll.id
-  name        = "allow-ssh"
-  description = "security group that allows ssh and all egress traffic"
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  tags = {
-    Name = "allow-all-tcp"
-  }
-}
-
