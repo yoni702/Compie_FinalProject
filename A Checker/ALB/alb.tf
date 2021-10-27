078c46f28a1bf9085ec2571d0b8c71afc200ca49
Yonathan Zerbib <50784530+yoni702@users.noreply.github.com>
2021-10-27 12:44:47 +0300

    v1

---
+++ A Checker/ALB/alb.tf
@@ -0,0 +1,80 @@
+resource "aws_lb" "albmasteroll" {
+  name               = "Masteroll"
+  internal           = false
+  load_balancer_type = "application"
+  #Chech this line
+  security_groups    = [aws_security_group.lb_sg.id]
+  
+  subnet_mapping {
+    subnet_id     = aws_subnet.masteroll-private-1.id
+   
+  }
+
+  subnet_mapping {
+    subnet_id     = aws_subnet.masteroll-private-2.id
+    
+  }
+
+  tags = {
+    Environment = "production"
+  }
+}
+
+# ---------------------------aws_lb_listener-----------------------------------
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.front_end.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.front_end.arn
+  }
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.front_end.arn
+  port              = "80"
+  protocol          = "HTTPs"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.front_end.arn
+  }
+}
+
+# ---------------------------Instance_target_group-----------------------------------
+resource "aws_lb_target_group" "test" {
+  name     = "tf-example-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+
+ #---------------------------Target_group_attachment-----------------------------------
+
+
+resource "aws_lb_target_group_attachment" "test" {
+  target_group_arn = aws_lb_target_group.test.arn
+  target_id        = aws_instance.test.id
+  port             = 80
+}
+
+resource "aws_lb_target_group" "test" {
+  # ... other configuration ...
+}
+
+resource "aws_instance" "test" {
+  # ... other configuration ...
+  #aws_instance.WebServer1
+  #aws_instance.WebServer2
+}
