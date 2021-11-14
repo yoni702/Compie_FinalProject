afa469fba671c7a5f0732481d167f2e9ab4f9cc6
Yonathan Zerbib <50784530+yoni702@users.noreply.github.com>
2021-11-14 10:46:08 +0200

    s

--- A_Checker/EKS /EKS TERRAFORM/eks.tf
+++ A_Checker/EKS /EKS TERRAFORM/eks.tf
@@ -1,31 +1,31 @@
-resource "aws_eks_cluster" "example" {
-  name     = "example"
-  role_arn = aws_iam_role.example.arn
+resource "aws_eks_cluster" "cluster" {
+  name     = "cluster"
+  role_arn = aws_iam_role.cluster.arn
 
   vpc_config {
-    subnet_ids = [aws_subnet.example1.id, aws_subnet.example2.id]
+    subnet_ids = [aws_subnet.masteroll-public-1.id, aws_subnet.masteroll-public-2.id]
   }
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
   # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
   depends_on = [
-    aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.example-AmazonEKSVPCResourceController,
+    aws_iam_role_policy_attachment.cluster-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.cluster-AmazonEKSVPCResourceController,
   ]
 }
 
 output "endpoint" {
-  value = aws_eks_cluster.example.endpoint
+  value = aws_eks_cluster.cluster.endpoint
 }
 
 output "kubeconfig-certificate-authority-data" {
-  value = aws_eks_cluster.example.certificate_authority[0].data
+  value = aws_eks_cluster.cluster.certificate_authority[0].data
 }
 
 #-------------IAM Role for EKS Cluster
 
-resource "aws_iam_role" "example" {
-  name = "eks-cluster-example"
+resource "aws_iam_role" "cluster" {
+  name = "eks-cluster-cluster"
 
   assume_role_policy = <<POLICY
 {
@@ -43,83 +43,51 @@ resource "aws_iam_role" "example" {
 POLICY
 }
 
-resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
+resource "aws_iam_role_policy_attachment" "cluster-AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.example.name
+  role       = aws_iam_role.cluster.name
 }
 
 # Optionally, enable Security Groups for Pods
 # Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
-resource "aws_iam_role_policy_attachment" "example-AmazonEKSVPCResourceController" {
+resource "aws_iam_role_policy_attachment" "cluster-AmazonEKSVPCResourceController" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
-  role       = aws_iam_role.example.name
-}
-
-
-
-#---------Enabling Control Plane Logging
-
-
-variable "cluster_name" {
-  default = "example"
-  type    = string
-}
-
-resource "aws_eks_cluster" "example" {
-  depends_on = [aws_cloudwatch_log_group.example]
-
-  enabled_cluster_log_types = ["api", "audit"]
-  name                      = var.cluster_name
-
-  # ... other configuration ...
-}
-
-resource "aws_cloudwatch_log_group" "example" {
-  # The log group name format is /aws/eks/<cluster-name>/cluster
-  # Reference: https://docs.aws.amazon.com/eks/latest/userguide/control-plane-logs.html
-  name              = "/aws/eks/${var.cluster_name}/cluster"
-  retention_in_days = 7
-
-  # ... potentially other configuration ...
+  role       = aws_iam_role.cluster.name
 }
 
 
 
 #-------------Enabling IAM Roles for Service Accounts
 
-resource "aws_eks_cluster" "example" {
-  # ... other configuration ...
+data "tls_certificate" "cluster" {
+  url = aws_eks_cluster.cluster.identity[0].oidc[0].issuer
 }
 
-data "tls_certificate" "example" {
-  url = aws_eks_cluster.example.identity[0].oidc[0].issuer
-}
-
-resource "aws_iam_openid_connect_provider" "example" {
+resource "aws_iam_openid_connect_provider" "cluster" {
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.example.certificates[0].sha1_fingerprint]
-  url             = aws_eks_cluster.example.identity[0].oidc[0].issuer
+  thumbprint_list = [data.tls_certificate.cluster.certificates[0].sha1_fingerprint]
+  url             = aws_eks_cluster.cluster.identity[0].oidc[0].issuer
 }
 
-data "aws_iam_policy_document" "example_assume_role_policy" {
+data "aws_iam_policy_document" "cluster_assume_role_policy" {
   statement {
     actions = ["sts:AssumeRoleWithWebIdentity"]
     effect  = "Allow"
 
     condition {
       test     = "StringEquals"
-      variable = "${replace(aws_iam_openid_connect_provider.example.url, "https://", "")}:sub"
+      variable = "${replace(aws_iam_openid_connect_provider.cluster.url, "https://", "")}:sub"
       values   = ["system:serviceaccount:kube-system:aws-node"]
     }
 
     principals {
-      identifiers = [aws_iam_openid_connect_provider.example.arn]
+      identifiers = [aws_iam_openid_connect_provider.cluster.arn]
       type        = "Federated"
     }
   }
 }
 
-resource "aws_iam_role" "example" {
-  assume_role_policy = data.aws_iam_policy_document.example_assume_role_policy.json
-  name               = "example"
+resource "aws_iam_role" "cluster" {
+  assume_role_policy = data.aws_iam_policy_document.cluster_assume_role_policy.json
+  name               = "cluster"
 }
---
+++ Part_2/004_terraform/eks-cluster.tf
@@ -0,0 +1,49 @@
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = local.cluster_name
+  cluster_version = "1.21"
+  subnets         = module.vpc.private_subnets
+
+  tags = {
+    Environment = "training"
+    GithubRepo  = "terraform-aws-eks"
+    GithubOrg   = "terraform-aws-modules"
+  }
+
+  vpc_id = module.vpc.vpc_id
+
+  workers_group_defaults = {
+    root_volume_type = "gp2"
+  }
+
+  worker_groups = [
+    {
+      name                          = "worker-group-1"
+      spot_price                    = "0.199"
+      instance_type                 = "t2.small"
+      additional_userdata           = "echo foo bar"
+      asg_max_size                  = 10
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
+      kubelet_extra_args            = "--node-labels=node.kubernetes.io/lifecycle=spot"
+      suspended_processes           = ["AZRebalance"]
+    },
+    {
+      name                          = "worker-group-2"
+      spot_price                     = "0.20"
+      instance_type                 = "t2.medium"
+      asg_max_size                  = 10
+      additional_userdata           = "echo foo bar"
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
+      kubelet_extra_args            = "--node-labels=node.kubernetes.io/lifecycle=spot"
+      suspended_processes           = ["AZRebalance"]
+    },
+  ]
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
