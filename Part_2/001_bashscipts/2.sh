ccb07ed18557da44318949228b287b8c470db22b
Yonathan Zerbib <50784530+yoni702@users.noreply.github.com>
2021-11-14 18:33:00 +0200

    ssss

--- Part_2/001_bashscipts/2.sh
+++ Part_2/001_bashscipts/2.sh
@@ -5,3 +5,9 @@ cd FinalProject/Part_2
 # Download jenkins 
 sudo docker pull jenkins/jenkins
 sudo docker run -p 11011:8080 --name=jenkins-master jenkins/jenkins
+ 
+# insert AWS CREDENTIALS in JENKINS
+# install terraform plugins on jenkins
+
+# 
+# install terraform plugins on jenkins
