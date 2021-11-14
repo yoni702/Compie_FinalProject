e509556d06dd7152b5450cddcb9f56d0506b35f0
Yonathan Zerbib <50784530+yoni702@users.noreply.github.com>
2021-11-14 18:40:16 +0200

    dd

--- Part_2/001_bashscipts/2.sh
+++ Part_2/001_bashscipts/2.sh
@@ -7,7 +7,8 @@ sudo docker pull jenkins/jenkins
 sudo docker run -p 11011:8080 --name=jenkins-master jenkins/jenkins
  
 # insert AWS CREDENTIALS in JENKINS
-# install terraform plugins on jenkins
+# install "Terraform" plugins on jenkins
+# name it "terraform_plugin" in Global Tool Configuration 
 
 # 
 # install terraform plugins on jenkins
