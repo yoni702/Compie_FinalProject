3e73e6003271614ed10279876539757456a12605
Yonathan Zerbib <50784530+yoni702@users.noreply.github.com>
2021-11-15 03:29:01 +0200

    ppppp

--- Part_3/003_terraform/provider.tf
+++ Part_3/003_terraform/provider.tf
@@ -1,6 +1,12 @@
 provider "aws" {
-  access_key = var.AWS_ACCESS_KEY
-  secret_key = var.AWS_SECRET_KEY
   region     = var.AWS_REGION
 }
 
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+  version                = "~> 1.9"
+}
--- Part_3/003_terraform/vpc.tf
+++ Part_3/003_terraform/vpc.tf
@@ -1,91 +1,92 @@
 # Internet VPC
-resource "aws_vpc" "partc" {
+resource "aws_vpc" "masteroll" {
   cidr_block           = "10.0.0.0/16"
   instance_tenancy     = "default"
   enable_dns_support   = "true"
   enable_dns_hostnames = "true"
   enable_classiclink   = "false"
   tags = {
-    Name = "partc"
+    Name = "masteroll"
   }
 }
 
 # Subnets
-resource "aws_subnet" "partc-public-1" {
-  vpc_id                  = aws_vpc.partc.id
+resource "aws_subnet" "masteroll-public-1" {
+  vpc_id                  = aws_vpc.masteroll.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = "true"
   availability_zone       = "eu-west-1a"
 
   tags = {
-    Name = "partc-public-1"
+    Name = "masteroll-public-1"
   }
 }
 
-resource "aws_subnet" "partc-public-2" {
-  vpc_id                  = aws_vpc.partc.id
+resource "aws_subnet" "masteroll-public-2" {
+  vpc_id                  = aws_vpc.masteroll.id
   cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = "true"
   availability_zone       = "eu-west-1b"
 
   tags = {
-    Name = "partc-public-2"
+    Name = "masteroll-public-2"
   }
 }
 
-resource "aws_subnet" "partc-private-1" {
-  vpc_id                  = aws_vpc.partc.id
-  cidr_block              = "10.0.4.0/24"
+
+resource "aws_subnet" "masteroll-private-1" {
+  vpc_id                  = aws_vpc.masteroll.id
+  cidr_block              = "10.0.3.0/24"
   map_public_ip_on_launch = "false"
   availability_zone       = "eu-west-1a"
 
   tags = {
-    Name = "partc-private-1"
+    Name = "masteroll-private-1"
   }
 }
-
-resource "aws_subnet" "partc-private-2" {
-  vpc_id                  = aws_vpc.partc.id
-  cidr_block              = "10.0.5.0/24"
+resource "aws_subnet" "masteroll-private-2" {
+  vpc_id                  = aws_vpc.masteroll.id
+  cidr_block              = "10.0.4.0/24"
   map_public_ip_on_launch = "false"
   availability_zone       = "eu-west-1b"
 
   tags = {
-    Name = "partc-private-2"
+    Name = "masteroll-private-2"
   }
 }
 
-
 # Internet GW
-resource "aws_internet_gateway" "partc-gw" {
-  vpc_id = aws_vpc.partc.id
+resource "aws_internet_gateway" "masteroll-gw" {
+  vpc_id = aws_vpc.masteroll.id
 
   tags = {
-    Name = "partc"
+    Name = "masteroll"
   }
 }
 
 # route tables
-resource "aws_route_table" "partc-public" {
-  vpc_id = aws_vpc.partc.id
+resource "aws_route_table" "masteroll-public" {
+  vpc_id = aws_vpc.masteroll.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.partc-gw.id
+    gateway_id = aws_internet_gateway.masteroll-gw.id
   }
 
   tags = {
-    Name = "partc-public-1"
+    Name = "masteroll-public-1"
   }
 }
 
 # route associations public
-resource "aws_route_table_association" "partc-public-1-a" {
-  subnet_id      = aws_subnet.partc-public-1.id
-  route_table_id = aws_route_table.partc-public.id
+resource "aws_route_table_association" "masteroll-public-1-a" {
+  subnet_id      = aws_subnet.masteroll-public-1.id
+  route_table_id = aws_route_table.masteroll-public.id
 }
 
-resource "aws_route_table_association" "partc-public-2-a" {
-  subnet_id      = aws_subnet.partc-public-2.id
-  route_table_id = aws_route_table.partc-public.id
+resource "aws_route_table_association" "masteroll-public-2-a" {
+  subnet_id      = aws_subnet.masteroll-public-2.id
+  route_table_id = aws_route_table.masteroll-public.id
 }
+
+
 
