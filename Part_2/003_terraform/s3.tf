93e44df5623b01bd2d75ba8e79251ccbd7fe5b9a
Yonathan Zerbib <50784530+yoni702@users.noreply.github.com>
2021-11-16 13:39:00 +0200

    dddd

--- Part_2/003_terraform/s3.tf
+++ Part_2/003_terraform/s3.tf
@@ -3,11 +3,15 @@
 #1 -this will create a S3 bucket in AWS
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "terraform-state"
+  region         = var.region
   force_destroy = true
+  lifecycle {
+    prevent_destroy = true
+  }
 # Enable versioning to see full revision history of our state files
   versioning {
-         enabled = true
-        }
+    enabled = true
+  }
 
 # Enable server-side encryption by default
 server_side_encryption_configuration {
@@ -24,7 +28,7 @@ terraform {
   backend "s3" {
     bucket         = "terraform-state"
     key            = "s3/terraform.tfstate"
-    region         = "us-east-2"
+    region         = var.region
     }
 }
 
