fd891ff1f27d3d0850e22fc623b4a5f426488676
Yonathan Zerbib <50784530+yoni702@users.noreply.github.com>
2021-11-16 14:46:22 +0200

    nmnmnm

--- Part_2/003_RemoteState/s3.tf
+++ Part_2/003_RemoteState/s3.tf
@@ -2,27 +2,15 @@ provider "aws" {
   region = "us-east-2"
 }
 
-#1 -this will create a S3 bucket in AWS
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-state"
-  region         = "us-east-2"
-  force_destroy = true
-  
-  lifecycle {
-    prevent_destroy = true
-  }
-# Enable versioning to see full revision history of our state files
+  bucket = "tfstate"
+
   versioning {
     enabled = true
   }
 
-# Enable server-side encryption by default
-server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
+  lifecycle {
+    prevent_destroy = true
   }
 }
 
--- Part_2/003_terraform/versions.tf
+++ Part_2/003_terraform/versions.tf
@@ -1,10 +1,10 @@
 terraform {
   #Creates S3 backend
   backend "s3" {
-    bucket         = "terraform-state"
-    key            = "s3/terraform.tfstate"
-    region         = var.region
-    }
+    bucket = "tfstate"
+    key = "app-state"
+    region = "us-east-1"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
