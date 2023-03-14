5a2c0a0307c7fb640d6392318e6516c91844fdca
yoni702 <yoni702@gmail.com>
2023-03-14 19:33:02 +0200

    a

--- Part_1/003_terraform/vars.tf
+++ Part_1/003_terraform/vars.tf
@@ -5,14 +5,14 @@ variable "AWS_SECRET_KEY" {
 }
 
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "eu-east-1"
 }
 
 variable "AMIS" {
   type = map(string)
   default = {
     us-east-2 = "ami-00399ec92321828f5"
-    us-west-2 = "ami-03d5c68bab01f3496"
+    us-east-1 = "ami-09e67e426f25ce0d7"
     eu-west-1 = "ami-0a8e758f5e873d1c1"
   }
 }
